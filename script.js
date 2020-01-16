a25d7ce2e2633059bd443d0874d69b929f9b1ba8
Louie Camacho <louiecamacho@Louies-MacBook-Pro-4.local>
2020-01-16 08:18:06 -0600

    modified styles

--- script.js
+++ script.js
@@ -1,24 +1,28 @@
 
-// const slider = document.querySelectorAll('.rgb-sliders input');
-const inputs = document.querySelectorAll('input');
+const sliders = document.querySelectorAll('input[type="range"]');
+const inputs = document.querySelectorAll('input[type="number"]');
 // const picker = document.querySelectorAll('.color-picker input');
 
 function updateColor() {
-	console.log(`${this.name}`, this.value);
+	// console.log(`${this.name}`, this.value);
 	document.documentElement.style.setProperty(`--${this.name}`, this.value);
 	
 	const results = document.querySelector(`input.${this.name}`);
 	results.value = `${this.value}`;
-
+	
+	for (let i = 0; i < sliders.length; i++) {
+		sliders[i].value = inputs[i].value;
+	}
 }
 
 // function customPicker() {
 // 	console.log(`${this.name}`, this.value);
 // 	document.documentElement.style.setProperty(`--${this.name}`, this.value);
 // }
-inputs.forEach( inputs => inputs.addEventListener('mousemove', updateColor) );
-inputs.forEach( inputs => inputs.addEventListener('change', updateColor) );
-inputs.forEach( inputs => inputs.addEventListener('input', updateColor) );
+
+sliders.forEach( slider => slider.addEventListener('mousemove', updateColor) );
+sliders.forEach( slider => slider.addEventListener('change', updateColor) );
+inputs.forEach( input => input.addEventListener('input', updateColor) );
 // textInput.forEach( textInput => textInput.addEventListener('input', updateColor) );
 // picker.forEach( picker => picker.addEventListener('change', customPicker) );
 
