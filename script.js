b85317dd7204d1f509708089df194d6044176302
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2020-01-24 17:17:19 -0600

    dec to hex conversion

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 
 const sliders = document.querySelectorAll('input[type="range"]');
 const inputs = document.querySelectorAll('input[type="number"]');
-// const picker = document.querySelectorAll('.color-picker input');
+const picker = document.querySelector('input[name="custom"]');
+const hexOut = document.querySelector('input.hex');
 
 function updateColor() {
-	// console.log(`${this.name}`, this.value);
-	document.documentElement.style.setProperty(`--${this.name}`, this.value);
+	let valStr = this.value;
+
+	console.log(`${this.name}`, valStr);
+	console.log(parseInt(valStr).toString(16)); //converts to hex value by first changing to decimal then re-convert back to string.
+
+	document.documentElement.style.setProperty(`--${this.name}`, valStr);
 	
 	const results = document.querySelector(`input.${this.name}`);
-	results.value = `${this.value}`;
+	results.value = `${valStr}`;
 	
 	for (let i = 0; i < sliders.length; i++) {
 		sliders[i].value = inputs[i].value;
@@ -26,5 +31,14 @@ sliders.forEach( slider => slider.addEventListener('change', updateColor) );
 inputs.forEach( input => input.addEventListener('input', updateColor) );
 // textInput.forEach( textInput => textInput.addEventListener('input', updateColor) );
 // picker.forEach( picker => picker.addEventListener('change', customPicker) );
+	
+// function updateHex() {
+// 	console.log(this.value);
+// 	const result = this.value;
+	
+// 	console.log(toString(result));
+// }
+
+// picker.addEventListener('change', updateHex);
 
 // todo
